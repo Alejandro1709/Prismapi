9b507827cc16880085ff55756a8afbbd49a86b95
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-12-08 21:14:13 -0500

    Add: users CRUD

--- src/server.js
+++ src/server.js
@@ -18,6 +18,18 @@ app.get('/api/v1/users', async (_req, res) => {
   res.status(200).json(users)
 })
 
+app.get('/api/v1/users/:id', async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: { id: Number(req.params.id) },
+  })
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  res.status(200).json(user)
+})
+
 app.post('/api/v1/users', async (req, res) => {
   const { name, email, username, password } = req.body
 
@@ -26,6 +38,31 @@ app.post('/api/v1/users', async (req, res) => {
   })
 
   res.status(201).json(user)
+})
+
+app.put('/api/v1/users/:id', async (req, res) => {
+  const { name, email, username, password } = req.body
+
+  const user = await prisma.user.update({
+    where: { id: Number(req.params.id) },
+    data: { name, email, username, password },
+  })
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  res.status(200).json({ message: 'User updated!' })
+})
+
+app.delete('/api/v1/users', async (req, res) => {
+  const user = await prisma.user.delete({ where: { id: req.params.id } })
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' })
+  }
+
+  res.status(200).json({ message: 'User deleted!' })
 })
 
 app.listen(2023, () => console.log('Server is running on port 2023'))
